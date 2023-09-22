8e8b1a0d7237afc2df488edbd0b2a89c683e1139
PrplFox <foxedex29@gmail.com>
2023-09-22 14:22:32 +0100

    handeled no items in the list

--- sites_scrapers/amazon.py
+++ sites_scrapers/amazon.py
@@ -6,22 +6,25 @@ from bs4 import BeautifulSoup as bs
 site_url = 'https://amazon.com/s?k=' 
 
 def amazon(searcharg):
-    search_argument = searcharg.replace(" ","+")
-    response = get_data(site_url, search_argument)
-    soup = bs(response.content, 'html.parser')
-    products = soup.select('[data-component-type="s-search-result"]')
-    
-    items = []
-    for product in products:
-        if len(items) >= 5: # This means we only need 5 items so the loop goes untile it fills the items list with 5 items
-            break
+    try:
+        search_argument = searcharg.replace(" ","+")
+        response = get_data(site_url, search_argument)
+        soup = bs(response.content, 'html.parser')
+        products = soup.select('[data-component-type="s-search-result"]')
         
-        title = product.find('span', class_='a-size-medium a-color-base a-text-normal')
-        price = product.find('span', class_='a-offscreen')
-        
-        if title and price:
-            items.append({'title':title.get_text(),'price':price.get_text()})
-        else:
-            pass
+        items = []
+        for product in products:
+            if len(items) >= 5: # This means we only need 5 items so the loop goes untile it fills the items list with 5 items
+                break
+            
+            title = product.find('span', class_='a-size-medium a-color-base a-text-normal')
+            price = product.find('span', class_='a-offscreen')
+            
+            if title and price:
+                items.append({'title':title.get_text(),'price':price.get_text()})
+            else:
+                pass
+    except:
+        return []
 
     return items
--- sites_scrapers/ebay.py
+++ sites_scrapers/ebay.py
@@ -6,23 +6,26 @@ from bs4 import BeautifulSoup as bs
 site_url = 'https://ebay.com/sch/i.html?_nkw='
 
 def ebay(searcharg):
-    search_argument = searcharg.replace(" ","+")
-    response = get_data(site_url, search_argument)
-    soup = bs(response.content, 'html.parser')
-    products = soup.find_all('div', class_='s-item__wrapper clearfix')
-    del products[0]
-    
-    items = []
-    for product in products:
-        if len(items) >= 5:# This means we only need 5 items so the loop goes untile it fills the items list with 5 items
-            break
+    try:
+        search_argument = searcharg.replace(" ","+")
+        response = get_data(site_url, search_argument)
+        soup = bs(response.content, 'html.parser')
+        products = soup.find_all('div', class_='s-item__wrapper clearfix')
+        del products[0]
         
-        title = product.find('div', class_='s-item__title')
-        price = product.find('span', class_='s-item__price')
-        
-        if title and price:
-            items.append({'title':title.get_text(),'price':price.get_text()})
-        else:
-            pass
+        items = []
+        for product in products:
+            if len(items) >= 5:# This means we only need 5 items so the loop goes untile it fills the items list with 5 items
+                break
+            
+            title = product.find('div', class_='s-item__title')
+            price = product.find('span', class_='s-item__price')
+            
+            if title and price:
+                items.append({'title':title.get_text(),'price':price.get_text()})
+            else:
+                pass
+    except:
+        return []
     
     return items
